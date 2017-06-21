f32aee11317a60b21a28c776340d7c02d0f8474c
Hope Atina <hopeatina@gmail.com>
2017-06-20 23:35:49 -0500

    Tunez page basic layout
    Need to adjust spacing/coloring/scroll/svg arrows
    Next Content / Styling / Routing /

---
+++ react-ui/src/Tunez/SongItem.js
@@ -0,0 +1,27 @@
+/**
+ * Created by theon on 6/6/2017.
+ */
+import React, {Component} from 'react';
+import '../App.css';
+// import reality from './reality.png'
+// import concepts from './concepts.png'
+// import tunez from './tunez.png'
+// import wrdz from './wrdz.png'
+import {Link, Route} from 'react-router-dom'
+
+class SongItem extends Component {
+
+    render() {
+
+        return (
+            <div className="Tunez-item">
+                <div className="Tunez-cover"></div>
+                <div className="Tunez-title">Tune Title</div>
+                <div className="Tunez-date">January 1, 2017</div>
+                <div className="Tunez-length">08:30</div>
+            </div>
+        )
+    }
+}
+
+export default SongItem
--- react-ui/src/Tunez/Tunez.js
+++ react-ui/src/Tunez/Tunez.js
@@ -1,7 +1,7 @@
 /**
  * Created by theon on 6/6/2017.
  */
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../App.css';
 // import reality from './reality.png'
 // import concepts from './concepts.png'
@@ -9,6 +9,7 @@ import '../App.css';
 // import wrdz from './wrdz.png'
 import {Link, Route} from 'react-router-dom'
 import Overlay from '../Overlay.js';
+import SongItem from './SongItem.js'
 
 
 class Tunez extends Component {
@@ -19,6 +20,24 @@ class Tunez extends Component {
             <div className="Tunez">
                 <Overlay/>
                 <div className="Pagetitle">TUNEZ</div>
+                <div className="Page-div">
+                    <div className="Tunez-list">
+                        <div className="Tunez-category">Songs</div>
+                        <SongItem/>
+                        <SongItem/>
+                        <SongItem/>
+                        <SongItem/>
+                        <SongItem/>
+                    </div>
+                    <div className="Tunez-playing">
+                        <div className="Tunez-songwave">
+                        </div>
+                        <div className="Tunez-ctrlback"></div>
+                        <div className="Tunez-play"></div>
+                        <div className="Tunez-ctrlnext"></div>
+
+                    </div>
+                </div>
             </div>
         );
     }
